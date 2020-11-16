e932d668aaa7e5fbcd54636ac790c99fcb06817b
Carlos <14030641@itcelaya.edu.mx>
2020-11-16 01:50:48 -0600

    SQLITE User Crud

--- lib/src/database/database_helper.dart
+++ lib/src/database/database_helper.dart
@@ -1,6 +1,59 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:practica2/src/models/user_dao.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
   static final _nombreBD = "PATM2020";
   static final _versionBD = 1;
+
+  static Database _database;
+  Future<Database> get database async {
+    if (_database != null) return _database;
+
+    _database = await _initDatabase();
+    return _database;
+  }
+
+  _initDatabase() async {
+    Directory carpeta = await getApplicationDocumentsDirectory();
+    String rutaBD = join(carpeta.path, _nombreBD);
+    return await openDatabase(
+      rutaBD,
+      version: _versionBD,
+      onCreate: _crearTablas,
+    );
+  }
+
+  _crearTablas(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE tbl_perfil(id INTEGER PRIMARY, nomUser varchar(25), apepUser varchar(25), apemUser varchar(25), telUser char(10), emailUser varchar(30), foto varchar (200), username varchar (30), pwduser varchar(30))");
+    //await db.execute("create...");
+  }
+
+  Future<int> insertar(Map<String, dynamic> row, String tabla) async {
+    var dbClient = await database;
+    return await dbClient.insert(tabla, row);
+  }
+
+  Future<int> actualizar(Map<String, dynamic> row, String tabla) async {
+    var dbClient = await database;
+    return await dbClient
+        .update(tabla, row, where: 'id = ?', whereArgs: [row['id']]);
+  }
+
+  Future<int> eliminar(int id, String tabla) async {
+    var dbClient = await database;
+    return await dbClient.delete(tabla, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<UserDAO> getUsuario(String emailUser) async {
+    var dbClient = await database;
+    var result = await dbClient
+        .query('tbl_perfil', where: 'emailUser = ?', whereArgs: [emailUser]);
+    var lista = (result).map((item) => UserDAO.fromJSON(item)).toList();
+    return lista[0];
+  }
 }
